ca7cacadf69b12711a70c8257b9dfa435a16e0f4
tamas <orosz.tamas@yahoo.com>
2022-03-29 09:13:50 +0200

    test: cryostat_surface

--- tests/test_superconductor_losses.py
+++ tests/test_superconductor_losses.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-from src.superconductor_losses import parallel_loss, perp_loss, norris_equation
+from src.superconductor_losses import parallel_loss, perp_loss, norris_equation, cryostat_losses, cryo_surface
+
+from math import pi
 
 
 class TestLosses(TestCase):
@@ -19,3 +21,11 @@ class TestLosses(TestCase):
         self.assertAlmostEqual(perp_loss(1.35, 50, 4.29 * 1e-3, 0.011, 50. * 1e-3), 1.18764, 3)
         # norris equation
         self.assertAlmostEqual(norris_equation(50, 50, 115.), 0.0047, 4)
+
+    def test_cryostat_losses(self):
+        r_in = 0.5 / pi
+        r_ou = 1.0 / pi
+        h = 1.0
+        a_cs = cryo_surface(r_in, r_ou, h)  # [m2]
+
+        self.assertAlmostEqual(a_cs, 3.238, 2)
